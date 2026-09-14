f849df37b7fb8d8649e4a25bbfdb46197f2afb13
OG <158096201+ogeidlife@users.noreply.github.com>
2026-09-14 18:33:23 -0300

    teste

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Cole aqui as chaves do SEU projeto Firebase.
 // Firebase Console > Configurações do projeto > Geral > "Seus apps" > Configuração do SDK
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyCQyqMdHHXFna0cOdLdu2AxjElzf4wJZZ0",
+  authDomain: "driveuv-8a020.firebaseapp.com",
+  projectId: "driveuv-8a020",
+  storageBucket: "driveuv-8a020.firebasestorage.app",
+  messagingSenderId: "185244195683",
+  appId: "1:185244195683:web:3d13fad50e5f767045d156"
 };
 
 // E-mail interno usado para autenticar a equipe (não precisa ser um e-mail real).
